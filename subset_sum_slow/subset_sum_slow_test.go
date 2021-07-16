83e6b6ab8c1f8bba4f7c56e3ae362fa3b61f9c34
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-07-16 10:28:13 +0900

    create SubsetSumSlowTest

--- subset_sum_slow/subset_sum_slow_test.go
+++ subset_sum_slow/subset_sum_slow_test.go
@@ -5,10 +5,18 @@ import (
 	"testing"
 )
 
-func TestSubsetSumSlow_normal(t *testing.T) {
+func TestSubsetSumSlowY_normal(t *testing.T) {
 	yn, x := SubsetSumSlow([]int{3, 7, 5, 8, 2}, 11)
 
-	if yn != Y || reflect.DeepEqual(x, []int{1, 0, 0, 1, 0}) {
-		t.Errorf("")
+	if yn != Y || !reflect.DeepEqual(x, []bool{true, false, false, true, false}) {
+		t.Errorf("SubsetSumSlow([]int{3, 7, 5, 8, 2}, 11) = %s, %v; want %s, [true, false, false, true, false]", yn, x, Y)
+	}
+}
+
+func TestSubsetSumSlowN_normal(t *testing.T) {
+	yn, x := SubsetSumSlow([]int{3, 7, 5, 8, 2}, 999)
+
+	if yn != N || len(x) != 0 {
+		t.Errorf("SubsetSumSlow([]int{3, 7, 5, 8, 2}, 999) = %s, %v; want %s, []", yn, x, N)
 	}
 }
