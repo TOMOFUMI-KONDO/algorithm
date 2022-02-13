adfb896d27344c1275005c62379f20b2ecf76313
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-02-13 09:39:46 +0900

    refactor,test

--- dijkstra/main.go
+++ dijkstra/main.go
@@ -1,5 +1,7 @@
 package dijkstra
 
+import "fmt"
+
 type Node uint
 type Cost uint
 type Edge struct {
@@ -8,56 +10,86 @@ type Edge struct {
 	Cost
 }
 
-const MaxCost = ^Cost(0)
-
-func (e Edge) opposite(n Node) (*Node, bool) {
+func (e Edge) opposite(n Node) Node {
 	if n == e.Node1 {
-		return &e.Node2, true
+		return e.Node2
 	}
 	if n == e.Node2 {
-		return &e.Node1, true
+		return e.Node1
 	}
-	return nil, false
+	panic(fmt.Errorf("node %d is not included in edge %#v", n, e))
 }
 
-func Dijkstra(nodes []Node, edges []Edge, start Node) []Edge {
-	p := map[Node]bool{start: true}
-	for _, v := range nodes {
-		if v != start {
-			p[v] = false
-		}
-	}
-	e := make(map[Node]Edge)
-	d := map[Node]Cost{start: 0}
-	for _, v := range nodes {
-		if v != start {
-			d[v] = MaxCost
-		}
-	}
-	u := start
-	var t []Edge
+const MaxCost = ^Cost(0)
 
-	for len(p) != len(nodes) {
-		for _, v := range edges {
-			n, ok := v.opposite(u)
-			if ok && !p[*n] {
-				if d[u]+v.Cost < d[*n] {
-					d[*n] = d[u] + v.Cost
-					e[*n] = v
-				}
+func Dijkstra(nodes []Node, edges []Edge, src Node, dst Node) []Edge {
+	visited := make(map[Node]bool, len(nodes))
+	for _, v := range nodes {
+		visited[v] = false
+	}
+	visited[src] = true
+
+	cost := make(map[Node]Cost, len(nodes))
+	for _, v := range nodes {
+		cost[v] = MaxCost
+	}
+	cost[src] = 0
+
+	neighbor := make(map[Node][]struct {
+		node Node
+		edge Edge
+	}, len(nodes))
+	for _, v := range edges {
+		neighbor[v.Node1] = append(neighbor[v.Node1], struct {
+			node Node
+			edge Edge
+		}{v.Node2, v})
+		neighbor[v.Node2] = append(neighbor[v.Node2], struct {
+			node Node
+			edge Edge
+		}{v.Node1, v})
+	}
+
+	edgeToNode := make(map[Node]Edge)
+	lastVisited := src
+	var tree []Edge
+
+	for !visited[dst] {
+		// update neighbor costs
+		for _, v := range neighbor[lastVisited] {
+			if visited[v.node] {
+				continue
+			}
+			if cost[lastVisited]+v.edge.Cost < cost[v.node] {
+				cost[v.node] = cost[lastVisited] + v.edge.Cost
+				edgeToNode[v.node] = v.edge
 			}
 		}
+
 		var minNode Node
 		minCost := MaxCost
-		for k, v := range d {
-			if !p[k] && v < minCost {
-				minNode = k
-				minCost = v
+		for n, c := range cost {
+			if !visited[n] && c < minCost {
+				minNode = n
+				minCost = c
 			}
 		}
-		p[minNode] = true
-		t = append(t, e[minNode])
+		lastVisited = minNode
+		visited[minNode] = true
+		tree = append(tree, edgeToNode[minNode])
 	}
 
-	return t
+	var path []Edge
+	cur := dst
+	for {
+		fmt.Println(edgeToNode[cur])
+		path = append([]Edge{edgeToNode[cur]}, path...)
+		oppo := edgeToNode[cur].opposite(cur)
+		if oppo == src {
+			break
+		}
+		cur = oppo
+	}
+
+	return path
 }
