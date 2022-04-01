67ca2f2cd503e4aba9ab2a0d4d82f77096c38ad4
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-04-01 21:10:07 +0900

    test: add test case

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -2,16 +2,27 @@ package sort
 
 import "testing"
 
+var sorts = []Sort{BubbleSort, QuickSort}
+var tests = []struct {
+	in   []int
+	want []int
+}{
+	{in: []int{1, 2, 3, 4, 5}, want: []int{1, 2, 3, 4, 5}},
+	{in: []int{5, 3, 9, 7, 6, 1}, want: []int{1, 3, 5, 6, 7, 9}},
+	{in: []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}, want: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
+}
+
 func TestBubbleSort(t *testing.T) {
-	sorts := []Sort{BubbleSort, QuickSort}
 
 	for _, sort := range sorts {
-		l := []int{5, 3, 9, 7, 6, 1}
-		sort(&l)
+		for _, test := range tests {
+			sort(&test.in)
 
-		if !same(l, []int{1, 3, 5, 6, 7, 9}) {
-			t.Errorf("l = %v; want [1, 3, 5, 6, 7, 9]", l)
+			if !same(test.in, test.want) {
+				t.Errorf("l = %v; want [1, 3, 5, 6, 7, 9]", test.in)
+			}
 		}
+
 	}
 }
 
