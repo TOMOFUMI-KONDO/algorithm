941501de6c9b58ee947fc79281e6dce45cdf0820
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-04-01 20:15:34 +0900

    refactor

--- sort/bubble_sort.go
+++ sort/bubble_sort.go
@@ -1,12 +1,10 @@
-package bubblesort
+package sort
 
-func sort(l []int) {
+func BubbleSort(l []int) {
 	for i := len(l) - 1; i > 0; i-- {
 		for j := 0; j < i; j++ {
 			if l[j] > l[j+1] {
-				tmp := l[j]
-				l[j] = l[j+1]
-				l[j+1] = tmp
+				l[j], l[j+1] = l[j+1], l[j]
 			}
 		}
 	}
